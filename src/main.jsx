fc1a72a749f300475e0cce04ede2a339dd630157
juanmagdev <me@juanmagd.dev>
2023-01-27 12:33:00 +0100

    Routes fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { HooksApp } from "./HooksApp";
-import { SimpleForm } from "./01-useState/02-useEffect/SimpleForm";
+import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 import './index.css'
 
